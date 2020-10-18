b11ed4b74dc36ca9ea32d8a6064a5d8c76693421
Brian DeRocher <brian@derocher.org>
2020-10-18 11:36:23 -0400

    add election info

--- pages/election_2020.vue
+++ pages/election_2020.vue
@@ -21,10 +21,33 @@
           <td>Libertarian</td>
         </tr>
       </table>
+      <h2>US Senate</h2>
+      <table>
+        <tr>
+          <th>Candidates</th>
+          <th>Party</th>
+        </tr>
+        <tr>
+          <td>Daniel Gade</td>
+          <td>Republican</td>
+        </tr>
+        <tr>
+          <td>Mark Warner</td>
+          <td>Democrat</td>
+        </tr>
+        <tr>
+          <td>Al Mina</td>
+          <td>independent</td>
+        </tr>
+      </table>
     </section>
     <section>
       <h1>State</h1>
       <h2>Referrenda</h2>
+      <ul>
+        <li>Redistricting Commission Amendment</li>
+        <li>Motor Vehicle Property Tax Exemption for Disabled Veterans Amendment</li>
+      </ul>
     </section>
     <section>
       <h1>Local</h1>

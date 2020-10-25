df1072da0f1b6b7bc060ed39ea613c390ee38ec7
Brian DeRocher <brian@derocher.org>
2020-10-25 11:36:23 -0400

    Use vuetify via yarn and buildModule.

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    '@nuxtjs/vuetify',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)

8bef48cebb1c53458bbee9cbd6c3dc18c7854fc9
Brian DeRocher <brian@derocher.org>
2020-10-25 00:52:37 -0400

    Change logo to flag.  Remove some green styles.

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,22 +1,33 @@
 <template>
-  <svg class="NuxtLogo" width="245" height="180" viewBox="0 0 452 342" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M139 330l-1-2c-2-4-2-8-1-13H29L189 31l67 121 22-16-67-121c-1-2-9-14-22-14-6 0-15 2-22 15L5 303c-1 3-8 16-2 27 4 6 10 12 24 12h136c-14 0-21-6-24-12z"
-      fill="#00C58E"
-    />
-    <path
-      d="M447 304L317 70c-2-2-9-15-22-15-6 0-15 3-22 15l-17 28v54l39-67 129 230h-49a23 23 0 0 1-2 14l-1 1c-6 11-21 12-23 12h76c3 0 17-1 24-12 3-5 5-14-2-26z"
-      fill="#108775"
-    />
-    <path
-      d="M376 330v-1l1-2c1-4 2-8 1-12l-4-12-102-178-15-27h-1l-15 27-102 178-4 12a24 24 0 0 0 2 15c4 6 10 12 24 12h190c3 0 18-1 25-12zM256 152l93 163H163l93-163z"
-      fill="#2F495E"
-    />
+  <svg class="FlagLogo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1235" height="650" viewBox="0 0 7410 3900">
+    <rect width="7410" height="3900" fill="#b22234"/>
+    <path d="M0,450H7410m0,600H0m0,600H7410m0,600H0m0,600H7410m0,600H0" stroke="#fff" stroke-width="300"/>
+    <rect width="2964" height="2100" fill="#3c3b6e"/>
+    <g fill="#fff">
+      <g id="s18">
+        <g id="s9">
+          <g id="s5">
+            <g id="s4">
+              <path id="s" d="M247,90 317.534230,307.082039 132.873218,172.917961H361.126782L176.465770,307.082039z"/>
+              <use xlink:href="#s" y="420"/>
+              <use xlink:href="#s" y="840"/>
+              <use xlink:href="#s" y="1260"/>
+            </g>
+            <use xlink:href="#s" y="1680"/>
+          </g>
+          <use xlink:href="#s4" x="247" y="210"/>
+        </g>
+        <use xlink:href="#s9" x="494"/>
+      </g>
+      <use xlink:href="#s18" x="988"/>
+      <use xlink:href="#s9" x="1976"/>
+      <use xlink:href="#s5" x="2470"/>
+    </g>
   </svg>
 </template>
 
 <style>
-.NuxtLogo {
+.FlagLogo {
   animation: 1s appear;
   margin: auto;
 }

a75241b496abb5152e53bc0594abb60a77ec9394
Brian DeRocher <brian@derocher.org>
2020-10-25 11:36:49 -0400

    Use v-simple-table for candidates.

--- pages/election_2020.vue
+++ pages/election_2020.vue
@@ -3,43 +3,46 @@
     <section>
       <h1>National</h1>
       <h2>President</h2>
-      <table>
-        <tr>
-          <th>Candidates</th>
-          <th>Party</th>
-        </tr>
-        <tr>
-          <td>Donald Trump / Mike Pence</td>
-          <td>Republican</td>
-        </tr>
-        <tr>
-          <td>Joe Biden / Kamala Harris</td>
-          <td>Democrat</td>
-        </tr>
-        <tr>
-          <td>Jo Jorgensen / Spike Cohen</td>
-          <td>Libertarian</td>
-        </tr>
-      </table>
-      <h2>US Senate</h2>
-      <table>
-        <tr>
-          <th>Candidates</th>
-          <th>Party</th>
-        </tr>
-        <tr>
-          <td>Daniel Gade</td>
-          <td>Republican</td>
-        </tr>
-        <tr>
-          <td>Mark Warner</td>
-          <td>Democrat</td>
-        </tr>
-        <tr>
-          <td>Al Mina</td>
-          <td>independent</td>
-        </tr>
-      </table>
+      <v-sheet width="400">
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+            <tr>
+              <th>Candidates</th>
+              <th>Party</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in pres_candidates" :key="item.name">
+              <td>{{ item.name }}</td>
+              <td>{{ item.party }}</td>
+            </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-sheet>
+      <p>
+        <nuxt-link :to="{name: 'election-tracker_president'}">presidential election tracker</nuxt-link>
+      </p>
+      <h2>US Senate - Virginia</h2>
+      <v-sheet width="400">
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+            <tr>
+              <th>Candidate</th>
+              <th>Party</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in senate_va_candidates" :key="item.name">
+              <td>{{ item.name }}</td>
+              <td>{{ item.party }}</td>
+            </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-sheet>
     </section>
     <section>
       <h1>State</h1>
@@ -54,3 +57,39 @@
     </section>
   </section>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      pres_candidates: [
+        {
+          name: 'Donald Trump / Mike Pence',
+          party: 'Republican',
+        },
+        {
+          name: 'Joe Biden / Kamala Harris',
+          party: 'Democratic',
+        },
+        {
+          name: 'Jo Jorgensen / Spike Cohen',
+          party: 'Libertarian',
+        },
+      ],
+      senate_va_candidates: [
+        {
+          name: 'Daniel Gade',
+          party: 'Republican',
+        },
+        {
+          name: 'Mark Warner',
+          party: 'Democratic',
+        },
+        {
+          name: 'Al Mina',
+          party: 'independent',
+        },
+      ],
+    }
+  },
+}
+</script>

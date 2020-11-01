672794f5861e0d63bc89ed52e2b7cd4f2c20b315
Brian DeRocher <brian@derocher.org>
2020-11-01 09:38:21 -0500

    whitespace

--- pages/election/2020/senate_tracker.vue
+++ pages/election/2020/senate_tracker.vue
@@ -50,10 +50,7 @@ export default {
     let st = [
       {
         id: "Alabama",
-        incumbent: {
-          name: "Doug Jones",
-          party: "D",
-        },
+        incumbent: { name: "Doug Jones", party: "D" },
         candidates: [
           {
             name: "",
@@ -63,241 +60,139 @@ export default {
       },
       {
         id: "Alaska",
-        incumbent: {
-          name: "Dan Sullivan",
-          party: "R",
-        },
+        incumbent: { name: "Dan Sullivan", party: "R" },
       },
       {
         id: "Arizona Sp",
-        incumbent: {
-          name: "Martha McSally",
-          party: "R",
-        },
+        incumbent: { name: "Martha McSally", party: "R" },
       },
       {
         id: "Arkansas",
-        incumbent: {
-          name: "Tom Cotton",
-          party: "R",
-        },
+        incumbent: { name: "Tom Cotton", party: "R" },
       },
       {
         id: "Colorado",
-        incumbent: {
-          name: "Cory Gdner",
-          party: "R",
-        },
+        incumbent: { name: "Cory Gdner", party: "R" },
       },
       {
         id: "Delaware",
-        incumbent: {
-          name: "Chris Coons",
-          party: "D",
-        },
+        incumbent: { name: "Chris Coons", party: "D" },
       },
       {
         id: "Georgia Reg",
-        incumbent: {
-          name: "David Perdue",
-          party: "R",
-        },
+        incumbent: { name: "David Perdue", party: "R" },
       },
       {
         id: "Georgia Sp",
-        incumbent: {
-          name: "Kelly Loeffler",
-          party: "R",
-        },
+        incumbent: { name: "Kelly Loeffler", party: "R" },
       },
       {
         id: "Idaho",
-        incumbent: {
-          name: "Jim Risch",
-          party: "R",
-        },
+        incumbent: { name: "Jim Risch", party: "R" },
       },
       {
         id: "Illinois",
-        incumbent: {
-          name: "Dick Durbin",
-          party: "D",
-        },
+        incumbent: { name: "Dick Durbin", party: "D" },
       },
       {
         id: "Iowa",
-        incumbent: {
-          name: "Joni Ernst",
-          party: "R",
-        },
+        incumbent: { name: "Joni Ernst", party: "R" },
       },
       {
         id: "Kansas",
-        incumbent: {
-          name: "Pat Roberts",
-          party: "R",
-        },
+        incumbent: { name: "Pat Roberts", party: "R" },
       },
       {
         id: "Kentucky",
-        incumbent: {
-          name: "Mitch McConnell",
-          party: "R",
-        },
+        incumbent: { name: "Mitch McConnell", party: "R" },
       },
       {
         id: "Louisiana",
-        incumbent: {
-          name: "Bill Cassidy",
-          party: "R",
-        },
+        incumbent: { name: "Bill Cassidy", party: "R" },
       },
       {
         id: "Maine",
-        incumbent: {
-          name: "Susan Collins",
-          party: "R",
-        },
+        incumbent: { name: "Susan Collins", party: "R" },
       },
       {
         id: "Massachusetts",
-        incumbent: {
-          name: "Ed Markey",
-          party: "D",
-        },
+        incumbent: { name: "Ed Markey", party: "D" },
       },
       {
         id: "Michigan",
-        incumbent: {
-          name: "Gary Peters",
-          party: "D",
-        },
+        incumbent: { name: "Gary Peters", party: "D" },
       },
       {
         id: "Minnesota",
-        incumbent: {
-          name: "Tina Smith",
-          party: "D",
-        },
+        incumbent: { name: "Tina Smith", party: "D" },
       },
       {
         id: "Mississippi",
-        incumbent: {
-          name: "Cindy Hyde-Smith",
-          party: "R",
-        },
+        incumbent: { name: "Cindy Hyde-Smith", party: "R" },
       },
       {
         id: "Montana",
-        incumbent: {
-          name: "Steve Daines",
-          party: "R",
-        },
+        incumbent: { name: "Steve Daines", party: "R" },
       },
       {
         id: "Nebraska",
-        incumbent: {
-          name: "Ben Sasse",
-          party: "R",
-        },
+        incumbent: { name: "Ben Sasse", party: "R" },
       },
       {
         id: "New Hampshire",
-        incumbent: {
-          name: "Jeanne Shaheen",
-          party: "D",
-        },
+        incumbent: { name: "Jeanne Shaheen", party: "D" }
       },
       {
         id: "New Jersey",
-        incumbent: {
-          name: "Cory Booker",
-          party: "D",
-        },
+        incumbent: { name: "Cory Booker", party: "D" },
       },
       {
         id: "New Mexico",
-        incumbent: {
-          name: "Tom Udall",
-          party: "D",
-        },
+        incumbent: { name: "Tom Udall", party: "D" },
       },
       {
         id: "North Carolina",
-        incumbent: {
-          name: "Thom Tillis",
-          party: "R",
-        },
+        incumbent: { name: "Thom Tillis", party: "R" },
       },
       {
         id: "Oklahoma",
-        incumbent: {
-          name: "Jim Inhofe",
-          party: "R",
-        },
+        incumbent: { name: "Jim Inhofe", party: "R" },
       },
       {
         id: "Oregon",
-        incumbent: {
-          name: "Jeff Merkley",
-          party: "D",
-        },
+        incumbent: { name: "Jeff Merkley", party: "D" },
       },
       {
         id: "Rhode Island",
-        incumbent: {
-          name: "Jack Reed",
-          party: "D",
-        },
+        incumbent: { name: "Jack Reed", party: "D" },
       },
       {
         id: "South Carolina",
-        incumbent: {
-          name: "Lindsey Graham",
-          party: "R",
-        },
+        incumbent: { name: "Lindsey Graham", party: "R" },
       },
       {
         id: "South Dakota",
-        incumbent: {
-          name: "Mike Rounds",
-          party: "R",
-        },
+        incumbent: { name: "Mike Rounds", party: "R" },
       },
       {
         id: "Tennessee",
-        incumbent: {
-          name: "Lamar Alaxander",
-          party: "R",
-        },
+        incumbent: { name: "Lamar Alaxander", party: "R" },
       },
       {
         id: "Texas",
-        incumbent: {
-          name: "John Cornyn",
-          party: "R",
-        },
+        incumbent: { name: "John Cornyn", party: "R" },
       },
       {
         id: "Virginia",
-        incumbent: {
-          name: "Mark Warner",
-          party: "D",
-        },
+        incumbent: { name: "Mark Warner", party: "D" },
       },
       {
         id: "West Virginia",
-        incumbent: {
-          name: "Shelley Moore Capito",
-          party: "R",
-        },
+        incumbent: { name: "Shelley Moore Capito", party: "R" },
       },
       {
         id: "Wyoming",
-        incumbent: {
-          name: "Mike Enzi",
-          party: "R",
-        },
+        incumbent: { name: "Mike Enzi", party: "R" },
       },
     ];
     function agg(st) {

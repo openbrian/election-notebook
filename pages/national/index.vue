b253c6acedcbcc1c8a5323c4c96bc835d387bd9c
Brian DeRocher <brian@derocher.org>
2020-11-02 13:24:50 -0500

    use v-list for national page

--- pages/national/index.vue
+++ pages/national/index.vue
@@ -2,23 +2,37 @@
   <section class="home">
     <h1 class="home__heading">3 Branches of Federal Government</h1>
     <div class="cards">
-      <div
-        class="card__container"
-        v-for="branch in branches"
-        :key="branch.id"
-        @click.prevent="storeBranchInfo(branch)"
-      >
-        <p class="card__name">
-          <nuxt-link
-            :to="{ name: 'national-id', params: { id: branch.id, dir: branch } }"
-          >{{ branch.name }}</nuxt-link>
-        </p>
-      </div>
+      <v-card elevation="5" min-width="250px" v-for="branch in branches">
+        <v-card-title>{{ branch.name }}</v-card-title>
+        <v-card-subtitle>{{ branch.info }}</v-card-subtitle>
+        <v-card-text>
+          <v-list>
+            <v-list-item v-for="a in branch.has">
+              <v-list-item-content>
+                <v-list-item-title>{{ a.name }}</v-list-item-title>
+                <v-list>
+                  <v-list-item v-for="b in a.has">
+                    <v-list-item-title>{{ b }}</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn>
+            <nuxt-link
+              :to="{ name: 'national-id', params: { id: branch.id, dir: branch } }"
+            >more info</nuxt-link>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </div>
+    <img alt="map of usa" src="~/assets/Map_of_USA_States_with_names_white.svg" />
+    <h2>Resources</h2>
     <p>
       <a href="https://www.usa.gov/branches-of-government">Branches of the U.S. Government</a>
     </p>
-    <img alt="map of usa" src="~/assets/Map_of_USA_States_with_names_white.svg" />
   </section>
 </template>
 <script>
@@ -31,17 +45,33 @@ export default {
         {
           id: "legislative",
           name: "Legislative",
-          info: "Makes laws"
+          info: "Makes laws",
+          has: [
+            {
+              name: "Congress",
+              has: ["Senate", "House of Representatives"],
+            }
+            ],
         },
         {
           id: "executive",
           name: "Executive",
-          info: "Carries out laws"
+          info: "Carries out laws",
+          has: [
+            {
+              name: "President",
+              has: ["Vice President", "Cabinet"],
+            }
+          ],
         },
         {
           id: "judicial",
           name: "Judicial",
-          info: "Interprets laws"
+          info: "Interprets laws",
+          has: [
+            { name: "Supreme Court", has: [] },
+            { name: "Other federal courts", has: [] },
+          ]
         },
       ]
     };
